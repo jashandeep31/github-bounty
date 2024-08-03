aa077ae097c5c31f4b0107cb37f21e5134b8d99d
jashandeep <jashandeep16592gmail.com>
2024-08-03 11:30:15 +0530

    feat: basic setup

---
+++ apps/user/components/navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Github bounty</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ apps/user/tailwind.config.ts
@@ -0,0 +1,6 @@
+import type { Config } from "tailwindcss";
+import tailwindWindConfig from "@repo/ui/tailwind.config";
+const config: Config = {
+  ...tailwindWindConfig,
+};
+export default config;
