35cb03edd2c944a77ceda3b1fb146cc0da640489
jashandeep <jashandeep1659@gmail.com>
2024-08-27 18:02:08 +0530

    fix: minor changes and fixes

--- apps/maintainer/app/(root)/layout.tsx
+++ apps/maintainer/app/(root)/layout.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "@/components/navbar";
+import NavbarMessageComponent from "@/components/navbar-message";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen justify-between">
-      <header>
+      <header className="sticky top-0 z-10">
         <Navbar />
       </header>
+      <NavbarMessageComponent />
+
       <main className="grid flex-1">{children}</main>
       <footer className=" py-4 border-t mt-12">
         <div className="container">
--- apps/maintainer/providers/_actions/index.ts
+++ apps/maintainer/providers/_actions/index.ts
@@ -1,21 +1,20 @@
 "use server";
 
 import { auth } from "@/lib/auth";
-import { verifyUserBasicAuthAndProperOrganization } from "@/lib/authentication";
+import { verifyUserBasicAuthAndBasicOrganizationValidation } from "@/lib/authentication";
 import { db } from "@/lib/db";
 
 export async function getOrganizationWalletAmount(): Promise<null | number> {
   const _session = await auth();
   const session = _session
-    ? verifyUserBasicAuthAndProperOrganization(_session)
+    ? verifyUserBasicAuthAndBasicOrganizationValidation(_session)
     : null;
-
   if (!session) return null;
   const organization = await db.organization.findUnique({
     where: {
       id: session.organization.id,
     },
   });
-  if (!organization?.balance) return null;
+  if (!organization) return null;
   return organization.balance;
 }
